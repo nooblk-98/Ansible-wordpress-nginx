b30a9011809c21f1a1a989676257e5ac97bdf619
Lahiru Liyanage <liyanagelsofficial@gmail.com>
2025-10-03 09:31:32 +0530

    update

--- infra/main.tf
+++ infra/main.tf
@@ -50,6 +50,13 @@ resource "aws_security_group" "web" {
   }
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
